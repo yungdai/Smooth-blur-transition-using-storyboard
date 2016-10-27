74ac2b3ef98e2d5175bf20d63e31b418ed667560
Yung Dai <yungchidai@gmail.com>
2016-10-26 20:49:48 -0400

    Initial Commit

---
+++ Smooth blur transition using storyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smooth blur transition using storyboard
+//
+//  Created by Yung Dai on 2016-10-26.
+//  Copyright © 2016 Yung Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Smooth blur transition using storyboardTests/Smooth_blur_transition_using_storyboardTests.swift
@@ -0,0 +1,36 @@
+//
+//  Smooth_blur_transition_using_storyboardTests.swift
+//  Smooth blur transition using storyboardTests
+//
+//  Created by Yung Dai on 2016-10-26.
+//  Copyright © 2016 Yung Dai. All rights reserved.
+//
+
+import XCTest
+@testable import Smooth_blur_transition_using_storyboard
+
+class Smooth_blur_transition_using_storyboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
